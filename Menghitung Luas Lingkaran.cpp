d4082714dec807ae9ecd96d3cf7d9a9e11fa9405
roni46 <39713502+roni46@users.noreply.github.com>
2019-01-03 17:35:10 +0700

    Add files via upload

---
+++ Menghitung Luas Lingkaran.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+int r;
+float phi=3.14,luas;
+printf("PROGRAM MENGHITUNG LUAS LINGKARAN\n");
+printf("BY M Achmad Sahroni NIM 311620782\n");
+printf("KAMPUS PELITA BANGSA. TEKNIK INFORMATIKA\n\n");
+
+cout<<"masukan jari jari lingkaran:";
+cin>>r;
+
+luas=phi*r*r;
+cout<<"luas lingkaran adalah "<<luas;
+return 0;
+
+}
